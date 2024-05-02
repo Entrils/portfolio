5ab38cfd2efa927a83ed2aaab52afdf4cdac2964
Savelev Andrey <needway@list.ru>
2024-05-02 13:42:30 +0300

    new certificate slider
    fixed slider, added demo to yami nyami

---
+++ src/components/Certificates/CertificateSlider.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import styles from './Certificates.module.css'
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+
+export const CertificateSlider = ({data}) => {
+
+    const [slide, setSlide] = useState(0);
+
+    const nextSlide = () => {
+        setSlide(slide === data.length-1 ? 0 : slide+1);
+    }
+
+    const prevSlide = () => {
+        setSlide(slide === 0 ? data.length-1 : slide-1);
+    }
+
+  return (
+
+    <div className={styles.carousel}>
+    <FaChevronLeft className={styles.LeftArrow} onClick={prevSlide}/>
+        {data.map((item, index)=>{
+            return <img src={item.imageSrc} alt={item.title} key={index} className={slide === index ? styles.slide : styles.slideHdn}/>
+        })}
+          <FaChevronRight className={styles.RightArrow} onClick={nextSlide}/>
+    <span className={styles.indicators}>
+        {data.map((_, index) =>{
+           return ( 
+            <button key={index} 
+            onClick={() => setSlide(index)}
+            className={ slide === index ? styles.indicatorBtn : styles.indicatorBtnInactive}></button>
+        )
+        })}
+    </span>
+    </div>
+  )
+}
--- src/components/Certificates/Certificates.jsx
+++ src/components/Certificates/Certificates.jsx
@@ -1,65 +1,18 @@
-import React, { useEffect, useState, Children, cloneElement, useRef } from 'react'
+import React from 'react'
 import styles from "./Certificates.module.css";
-import { getImageUrl } from '../../utils';
-import certificates from "../../data/certificates.json";
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
-import { CSSTransition} from 'react-transition-group'
+import {certificates} from '../../data/certificates.json'
 
-export const Certificates = ({children}) => {
-  const CERT_WIDTH_FULL = 710;
-  const [pages, setPages] = useState([])
-  const [offset,setOffset] = useState([])
+import { CertificateSlider } from './CertificateSlider';
 
-  const handleLeftArrowClick = () =>{
-    //console.log('Left');
-    setOffset(currentOffset =>{
-      const newOffset = currentOffset + CERT_WIDTH_FULL;
-      //console.log(newOffset)
-      return Math.min(newOffset,0);
-    })
-  }
-
-  const handleRightArrowClick = () =>{
-    //console.log('Right');
-
-    setOffset((currentOffset) => {
-      const newOffset = currentOffset - CERT_WIDTH_FULL;
-
-      const maxOffset = -(CERT_WIDTH_FULL *(pages.length-1))
-      //console.log(newOffset)
-      return Math.max(newOffset,maxOffset);
-    })
- 
-  }
-
-  useEffect(()=>{
-    setPages(
-      certificates.map((certificate,id) => {
-        return <div key={id} className={styles.certificate}>
-            <div className={styles.certificateImageContainer}>
-              <img src={getImageUrl(certificate.imageSrc)} alt={certificate.title} />
-            </div>
-          </div>
-      })
-    )
-  },[])
+export const Certificates = () => {
   
   return (
     
     <section className={styles.container} id='certificates'>
       <h2 className={styles.title}>Сертификаты</h2>
-      <div className={styles.window}>
-        
-      <FaChevronLeft className={styles.LeftArrow} onClick={handleLeftArrowClick}/>
-      <div className={styles.content}>
-      <div className={styles.certificates}
-        style={{
-          transform: `translateX(${offset}px)`
-        }}>
-        {pages}</div>
-      </div>
-      <FaChevronRight className={styles.RightArrow} onClick={handleRightArrowClick}/>
-      </div>
+      <div className={styles.certificates}>
+        <CertificateSlider data={certificates}/>
+        </div>
     </section>
   );
 }
